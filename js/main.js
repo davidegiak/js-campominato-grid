2963c9e6d90573cbc4c2d96552ca05ab2df20109
Davide Di Giacomo <davidedigiacomo001@gmail.com>
2024-04-30 16:06:35 +0200

    .

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-let griglia = document.querySelectorAll(".container");
+let griglia = document.querySelectorAll("container");
 
 for (let i = 1; i <= 100; i++) {
     let casella = quadrato(i);
-    document.append(casella)
+    griglia.append(casella)
 }
 
 function quadrato(contenuto) {
     let quadrato = document.createElement("div");
     quadrato.classList.add("square");
-    // quadrato.innerText = contenuto;
+    quadrato.innerText = contenuto;
     return quadrato;
 }
 
