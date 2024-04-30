5910521a3c2008c027ec6ad882b36665cf0abbb2
Davide Di Giacomo <davidedigiacomo001@gmail.com>
2024-04-30 17:10:33 +0200

    bottone per giocare

--- js/main.js
+++ js/main.js
@@ -1,27 +1,33 @@
 const griglia = document.querySelector(".container");
 
-for (let i = 1; i <= 100; i++) {
-    // let casella = document.createElement("div");
-    // casella.classList.add("square", "red");
-    let casella = quadrato(i)
-    griglia.append(casella);
+const btn = document.querySelector("#btn");
 
-    // casella.addEventListener("click", function() {
-    //     casella.classList.add("white");
-    //     casella.classList.remove("red");
-    //     casella.append(i);
-    // })
+btn.addEventListener("click", function(){
+    for (let i = 1; i <= 100; i++) {
+        // let casella = document.createElement("div");
+        // casella.classList.add("square", "red");
+        let casella = quadrato(i)
+        griglia.append(casella);
+    
+        // casella.addEventListener("click", function() {
+        //     casella.classList.add("white");
+        //     casella.classList.remove("red");
+        //     casella.append(i);
+        // })
+    
+    }
+    
+    function quadrato(contenuto) {
+        let square = document.createElement("div");
+        square.classList.add("square", "red");
+        square.addEventListener("click", function(){
+            square.append(contenuto);
+            square.classList.add("white");
+            square.classList.remove("red");
+        } )
+        return square
+    }
 
-}
+})
 
-function quadrato(contenuto) {
-    let square = document.createElement("div");
-    square.classList.add("square", "red");
-    square.addEventListener("click", function(){
-        square.append(contenuto);
-        square.classList.add("white");
-        square.classList.remove("red");
-    } )
-    return square
-}
 
