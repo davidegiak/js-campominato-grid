28057c050377f02ed4da6a2134064a6065052a18
Davide Di Giacomo <davidedigiacomo001@gmail.com>
2024-04-30 16:54:36 +0200

    ...

--- js/main.js
+++ js/main.js
@@ -1,15 +1,23 @@
-const griglia = document.querySelectorAll("container");
+const griglia = document.querySelector(".container");
 
 for (let i = 1; i <= 100; i++) {
-    let casella = document.createElement("div");
-    casella.classList.add("square")
+    // let casella = document.createElement("div");
+    // casella.classList.add("square", "red");
+    let casella = quadrato(i)
     griglia.append(casella);
+
+    casella.addEventListener("click", function() {
+        casella.classList.add("white");
+        casella.classList.remove("red");
+        casella.append(i);
+
+    })
+
 }
 
-// function quadrato(contenuto) {
-//     let quadrato = document.createElement("div");
-//     quadrato.classList.add("square");
-//     quadrato.innerText = contenuto;
-//     return quadrato;
-// }
+function quadrato(contenuto) {
+    let casella = document.createElement("div");
+    casella.classList.add("square", "red");
+}
 
+let square = quadrato(i)
