4351379ae4579a824ba45514a3adca2a4ddd50a1
Davide Di Giacomo <davidedigiacomo001@gmail.com>
2024-05-02 10:00:46 +0200

    fix di tutte le "bug"

--- js/main.js
+++ js/main.js
@@ -20,6 +20,7 @@ btn.addEventListener("click", function(){
     function quadrato(contenuto) {
         let square = document.createElement("div");
         square.classList.add("square", "red");
+        btn.classList.add("full");
         square.addEventListener("click", function(){
             if (square.classList.contains("red"))
             square.append(contenuto);
@@ -29,8 +30,23 @@ btn.addEventListener("click", function(){
         return square
     }
 
+
+    btn.addEventListener("click", function(){
+        if (btn.classList.contains("full")){   
+        document.querySelector(".container").innerHTML = "";
+        btn.classList.remove("full");      
+        }
+        else {
+            for (let i = 1; i <= 100; i++) {
+                // let casella = document.createElement("div");
+                // casella.classList.add("square", "red");
+                let casella = quadrato(i)
+                griglia.append(casella);
+            }
+        }
+    })
+
     
 })
-
 
 
